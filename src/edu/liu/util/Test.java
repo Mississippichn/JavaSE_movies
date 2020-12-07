fc12817271d1191bf6e4a04dae90a62d51d704ac
splitk <liukechn@163.com>
2020-12-07 22:51:17 +0800

    Utill包中的封装方法，输入个数，是否可以输入为空后返回

--- src/edu/liu/util/Test.java
+++ src/edu/liu/util/Test.java
@@ -2,7 +2,7 @@ package edu.liu.util;
 
 public class Test {
     public static void main(String[] args) {
-        String s = ScannerUtill.readKeyboard(2, false);
+        String s = ScannerUtill.readKeyboard(5, true);
         System.out.println(s);
     }
 }
